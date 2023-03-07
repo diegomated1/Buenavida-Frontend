881ec11b437ee72ad9a979085ce32e7b8a75fe2a
diegomated1 <diegocardwr@gmail.com>
2023-03-07 11:10:14 -0500

    add user session

--- src/resources/js/User.js
+++ src/resources/js/User.js
@@ -3,59 +3,142 @@
 class User{
 
     constructor(){
+        this.isLogged = false;
         this.favorites = {};
         this.cart = {};
+        this.user = {};
         this.loadData();
     }
 
     loadData(){
+        const userInfo = localStorage.getItem('user');
+        if(userInfo){
+            this.isLogged = true;
+            this.user = JSON.parse(userInfo);
+        }else{
+            this.isLogged = false;
+            return;
+        }
         const favorites = localStorage.getItem('favorites');
         const cart = localStorage.getItem('cart');
         if(favorites){
-            this.favorites = JSON.parse(favorites);
+            let _favorites = JSON.parse(favorites);
+            if(!_favorites[this.user.email]){
+                _favorites[this.user.email] = {};
+            }
+            this.favorites = _favorites;
+            localStorage.setItem('favorites', JSON.stringify(_favorites));
         }else{
-            localStorage.setItem('favorites','{}');
+            localStorage.setItem('favorites',`{"${this.user.email}":{}}`);
         }
         if(cart){
-            this.cart = JSON.parse(cart);
+            let _cart = JSON.parse(cart);
+            if(!_cart[this.user.email]){
+                _cart[this.user.email] = {};
+            }
+            this._cart = _cart;
+            localStorage.setItem('cart', JSON.stringify(_cart));
         }else{
-            localStorage.setItem('favorites','{}');
+            localStorage.setItem('cart',`{"${this.user.email}":{}}`);
         }
     }
 
-    addCart(product, amount){
-        if(this.cart[product.id]){
-            this.cart[product.id].amount += amount;
+    addCart(product, amount, cb){
+        if(this.isLogged){
+            if(this.cart[this.user.email][product.id]){
+                this.cart[this.user.email][product.id].amount += amount;
+            }else{
+                this.cart[this.user.email][product.id] = {product, amount};
+            }
+            localStorage.setItem('cart', JSON.stringify(this.cart));
+            cb(this.cart[this.user.email][product.id].amount);
         }else{
-            this.cart[product.id] = {product, amount};
+            alert('No logged');
         }
-        localStorage.setItem('cart', JSON.stringify(this.cart));
-        return this.cart[product.id].amount;
     }
 
-    delCart(id, amount){
-        if(this.cart[id].amount == 1){
-            delete this.cart[id];
+    delCart(id, amount, cb){
+        if(this.isLogged){
+            if(this.cart[this.user.email][id].amount == 1){
+                delete this.cart[this.user.email][id];
+            }else{
+                this.cart[this.user.email][id].amount -= amount;
+            }
+            localStorage.setItem('cart', JSON.stringify(this.cart));
+            cb(this.cart[this.user.email][id].amount);
         }else{
-            this.cart[id].amount -= amount;
+            alert('No logged');
         }
-        localStorage.setItem('cart', JSON.stringify(this.cart));
-        return this.cart[id].amount;
     }
 
-    removeProductCart(id){
-        delete this.cart[id];
-        localStorage.setItem('cart', JSON.stringify(this.cart));
+    removeProductCart(id, cb){
+        if(this.isLogged){
+            delete this.cart[this.user.email][id];
+            localStorage.setItem('cart', JSON.stringify(this.cart));
+            cb();
+        }else{
+            alert('No logged');
+        }
     }
 
-    addFavorite(product){
-        this.favorites[product.id] = product;
-        localStorage.setItem('favorites', JSON.stringify(this.favorites));
+    addFavorite(product, cb){
+        if(this.isLogged){
+            this.favorites[this.user.email][product.id] = product;
+            localStorage.setItem('favorites', JSON.stringify(this.favorites));
+            cb();
+        }else{
+            alert('No logged');
+        }
     }
 
     removeFavorite(id){
-        delete this.favorites[id];
-        localStorage.setItem('favorites', JSON.stringify(this.favorites));
+        if(this.isLogged){
+            delete this.favorites[this.user.email][id];
+            localStorage.setItem('favorites', JSON.stringify(this.favorites));
+            cb();
+        }else{
+            alert('No logged');
+        }
+    }
+
+    login(email, password){
+        var users = JSON.parse(localStorage.getItem('users'));
+        if(!users || users[email]===undefined){
+            return{
+                status: false,
+                message: 'User not found'
+            }
+        }else if(users[email].password !== password){
+            return{
+                status: false,
+                message: 'Invalid password'
+            }
+        }else{
+            localStorage.setItem('user', JSON.stringify({...users[email], email}));
+            this.loadData();
+            return{
+                status: true
+            }
+        }
+    }
+
+    register(firstName, lastName, email, password){
+        var users = JSON.parse(localStorage.getItem('users'));
+        if(users && users[email]!==undefined){
+            return{
+                status: false,
+                message: 'Email already taken'
+            }
+        }else{
+            if(!users){
+                users = {};
+            }
+            users[email] = {firstName, lastName, password};
+            localStorage.setItem('users', JSON.stringify(users));
+            return{
+                status: true
+            }
+        }
     }
 
 }
